3f5a2da61c12165a4693ef2f622bfa82852ab5b9
“[firstname <[valid-email]>
2022-06-03 08:06:49 +0300

    first commit

---
+++ 100daysofcode/tuple.py
@@ -0,0 +1,29 @@
+fruits=("apple","banana","orange")
+print(fruits)
+
+tuple1=("apple","mango","watermelon")
+tuple2=(1,2,3,45,)
+tuple3=(True,False,False,True)
+print(tuple1)
+print(tuple2)
+print(tuple3)
+
+tuple1=("grapes","guava","pawpaw")
+print(len(tuple1))
+
+fruits=("apple")
+print(type(fruits))
+
+thistuple = ("apple", "banana", "cherry")
+print(thistuple[1])
+
+thistuple = ("apple", "banana", "cherry")
+y = ("orange",)
+thistuple += y
+print(thistuple)
+
+thistuple = ("nissan","mercedez","subaru")
+x = ("BMW",)
+thistuple += x
+print(thistuple)
+
